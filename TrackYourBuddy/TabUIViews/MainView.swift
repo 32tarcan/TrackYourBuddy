657885d82ec7eb57071767ab1224968cef70c155
Bahadır Tarcan <101280910+32tarcan@users.noreply.github.com>
2024-09-24 23:37:53 +0300

    MainView Demo design added.

--- TrackYourBuddy/TabUIViews/MainView.swift
+++ TrackYourBuddy/TabUIViews/MainView.swift
@@ -6,13 +6,58 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct MainView: View {
     var body: some View {
-        Text("Main View")
+        NavigationStack {
+            VStack {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 20) {
+                        ForEach(0..<2) { index in
+                            SelectablePhotoCard()
+                                .frame(width: 380, height: 300)
+                                .background(Color.gray)
+                                .cornerRadius(15)
+                                .shadow(radius: 5)
+                                .padding(.leading, -5)
+                        }
+                    }
+                    .padding()
+                }
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 20) {
+                        NavigationLink(destination: CardView(cardNumber: 3, notes: "Bu üçüncü kartın notları.", weightSelection: 15)) {
+                            Image(systemName: "gear")
+                                .frame(width: 380, height: 300)
+                                .background(Color.gray)
+                                .cornerRadius(15)
+                                .shadow(radius: 5)
+                                .padding(.leading, -5)
+                        }
+                        
+                        NavigationLink(destination: CardView(cardNumber: 4, notes: "Bu dördüncü kartın notları.", weightSelection: 25)) {
+                            Image(systemName: "gear")
+                                .resizable()
+                                .frame(width: 380, height: 300)
+                                .background(Color.gray)
+                                .cornerRadius(15)
+                                .shadow(radius: 5)
+                                .padding(.leading, -5)
+                        }
+                    }
+                    .padding()
+                }
+            }
+            .navigationTitle("Track Your Friend")
+        }
     }
+        
 }
 
 #Preview {
     MainView()
 }
+
+
